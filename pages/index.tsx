513fe079fcf0709270b829a13c47a61389c58f38
Francis Calizo <francisacalizo@gmail.com>
2021-06-10 06:20:40 -0400

    initial commit. Configure TS

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Influencer</title>
+        <meta name="description" content="Influencer Mockup App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
