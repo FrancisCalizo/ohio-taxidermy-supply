e67827a17696c3b556be8e38596821bd3ac55586
Francis Calizo <francisacalizo@gmail.com>
2021-06-10 21:29:08 -0400

    feat: implement react-slick, eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,7 +36,7 @@ module.exports = {
         // This rule is not compatible with Next.js's <Link /> components
         'jsx-a11y/anchor-is-valid': 'off',
 
-        '@typescript-eslint/no-unused-vars': ['error'],
+        '@typescript-eslint/no-unused-vars': ['warn'],
         '@typescript-eslint/no-explicit-any': 'off',
 
         '@typescript-eslint/explicit-module-boundary-types': 'off',
