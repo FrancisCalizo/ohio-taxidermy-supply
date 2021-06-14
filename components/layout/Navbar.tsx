6cb55e32e1a9355aef6368cd73f3698fde5cc2b8
francis <francis@aahcs.com>
2021-06-14 17:53:24 -0400

    feat: implement white navbar on top

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,7 +1,24 @@
 import React, { useEffect, useState, Fragment } from 'react';
-import { Transition } from 'react-transition-group';
+import { Transition, TransitionStatus } from 'react-transition-group';
 import styled from 'styled-components';
-// import scrollTo from "gatsby-plugin-smoothscroll"
+
+export const size = {
+  sm: '425px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1440px',
+};
+
+export const device = {
+  minSm: `(min-width: ${size.sm})`,
+  minMd: `(min-width: ${size.md})`,
+  minLg: `(min-width: ${size.lg})`,
+  minXl: `(min-width: ${size.xl})`,
+  maxSm: `(max-width: ${size.sm})`,
+  maxMd: `(max-width: ${size.md})`,
+  maxLg: `(max-width: ${size.lg})`,
+  maxXl: `(max-width: ${size.xl})`,
+};
 
 export default function Navbar() {
   const [isNavColored, setisNavColored] = useState(false);
@@ -29,22 +46,13 @@ export default function Navbar() {
     <Fragment>
       <Transition in={isNavColored} timeout={500}>
         {(state) => (
-          <Nav state={state} isNavColored={isNavColored} isHamburgerOpen={isHamburgerOpen}>
+          <Nav state={state} isHamburgerOpen={isHamburgerOpen}>
             <Container>
-              <LogoContainer isNavColored={isNavColored}>Logo Here</LogoContainer>
-              <Hamburger isNavColored={isNavColored} onClick={handleClick}>
-                <BurgerLine
-                  isNavColored={isNavColored}
-                  isHamburgerOpen={isHamburgerOpen}
-                ></BurgerLine>
-                <BurgerLine
-                  isNavColored={isNavColored}
-                  isHamburgerOpen={isHamburgerOpen}
-                ></BurgerLine>
-                <BurgerLine
-                  isNavColored={isNavColored}
-                  isHamburgerOpen={isHamburgerOpen}
-                ></BurgerLine>
+              <LogoContainer>Logo Here</LogoContainer>
+              <Hamburger onClick={handleClick}>
+                <BurgerLine isHamburgerOpen={isHamburgerOpen}></BurgerLine>
+                <BurgerLine isHamburgerOpen={isHamburgerOpen}></BurgerLine>
+                <BurgerLine isHamburgerOpen={isHamburgerOpen}></BurgerLine>
               </Hamburger>
               <NavLinks>
                 <NavLink>
@@ -78,16 +86,11 @@ export default function Navbar() {
       </Transition>
       <Transition in={isHamburgerOpen} timeout={0}>
         {(state) => (
-          <HamburgerLinks
-            state={state}
-            isHamburgerOpen={isHamburgerOpen}
-            isNavColored={isNavColored}
-          >
+          <HamburgerLinks state={state} isHamburgerOpen={isHamburgerOpen}>
             <HamburgerLink
               onClick={() => {
                 setIsHamburgerOpen(false);
               }}
-              isNavColored={isNavColored}
             >
               Home
             </HamburgerLink>
@@ -95,7 +98,6 @@ export default function Navbar() {
               onClick={() => {
                 setIsHamburgerOpen(false);
               }}
-              isNavColored={isNavColored}
             >
               Story
             </HamburgerLink>
@@ -103,7 +105,6 @@ export default function Navbar() {
               onClick={() => {
                 setIsHamburgerOpen(false);
               }}
-              isNavColored={isNavColored}
             >
               When And Where
             </HamburgerLink>
@@ -111,7 +112,6 @@ export default function Navbar() {
               onClick={() => {
                 setIsHamburgerOpen(false);
               }}
-              isNavColored={isNavColored}
             >
               Wedding Party
             </HamburgerLink>
@@ -119,7 +119,6 @@ export default function Navbar() {
               onClick={() => {
                 setIsHamburgerOpen(false);
               }}
-              isNavColored={isNavColored}
             >
               Gallery
             </HamburgerLink>
@@ -127,7 +126,6 @@ export default function Navbar() {
               onClick={() => {
                 setIsHamburgerOpen(false);
               }}
-              isNavColored={isNavColored}
             >
               Registry
             </HamburgerLink>
@@ -135,7 +133,6 @@ export default function Navbar() {
               onClick={() => {
                 setIsHamburgerOpen(false);
               }}
-              isNavColored={isNavColored}
             >
               FAQs
             </HamburgerLink>
@@ -143,7 +140,6 @@ export default function Navbar() {
               onClick={() => {
                 setIsHamburgerOpen(false);
               }}
-              isNavColored={isNavColored}
             >
               RSVP
             </HamburgerLink>
@@ -151,7 +147,6 @@ export default function Navbar() {
               onClick={() => {
                 setIsHamburgerOpen(false);
               }}
-              isNavColored={isNavColored}
             >
               Contact
             </HamburgerLink>
@@ -162,25 +157,15 @@ export default function Navbar() {
   );
 }
 
-export const Nav = styled.nav`
-  background-color: ${(props) => {
-    switch (true) {
-      case props.state === 'entering':
-      case props.state === 'entered':
-        return 'white';
-      case props.state !== 'entered' && props.state !== 'entering' && props.isHamburgerOpen:
-        return 'rgba(70, 70, 70, 0.75)';
-      default:
-        return 'transparent';
-    }
-  }};
-  opacity: ${(props) => (props.state === 'entering' ? 0.0 : 1.0)};
-  height: ${(props) => (props.state === 'entering' ? 0 : '85px')};
-  color: ${(props) => (props.isNavColored ? 'var(--header-gray)' : 'white')};
-  box-shadow: ${(props) =>
-    props.isNavColored ? '0 3px 1px -1px var(--heart)' : '0 4px 7px -7px #dff9fb'};
-  transition: height 500ms ease-in, opacity 100ms ease-in, background-color 100ms ease-in-out 100ms,
-    color 100ms ease-in 300ms, box-shadow 100ms ease-in 300ms;
+export const Nav = styled.nav<{ state: TransitionStatus }>`
+  background-color: white;
+  /* opacity: ${(props) => (props.state === 'entering' ? 0.0 : 1.0)}; */
+  /* height: ${(props) => (props.state === 'entering' ? 0 : '85px')}; */
+  color: gray;
+  /* box-shadow: ${(props) =>
+    props.isNavColored ? '0 3px 1px -1px var(--heart)' : '0 4px 7px -7px #dff9fb'}; */
+  /* transition: height 500ms ease-in, opacity 100ms ease-in, background-color 100ms ease-in-out 100ms,
+    color 100ms ease-in 300ms, box-shadow 100ms ease-in 300ms; */
   overflow: hidden;
   position: fixed;
   top: 0;
@@ -195,10 +180,24 @@ const Container = styled.div`
   max-width: var(--container);
   margin: 0 auto;
   height: 100%;
+
+  @media ${device.maxLg} {
+    margin: auto 3rem;
+  }
+  @media ${device.maxMd} {
+    margin: auto 1.2rem;
+  }
+  @media ${device.minLg} {
+    justify-content: space-around;
+  }
 `;
 
 const NavLinks = styled.div`
   display: none;
+
+  @media ${device.minLg} {
+    display: flex;
+  }
 `;
 
 const Hamburger = styled.div`
@@ -206,6 +205,10 @@ const Hamburger = styled.div`
   border-radius: 10px;
   padding: 0.4rem 0.7rem;
   cursor: pointer;
+
+  @media ${device.minLg} {
+    display: none;
+  }
 `;
 
 const HamburgerLinks = styled.div`
@@ -219,35 +222,37 @@ const HamburgerLinks = styled.div`
   flex-wrap: wrap;
   text-transform: uppercase;
   font-weight: 700;
-  background: ${(props) => (props.isNavColored ? 'white' : 'rgba(70,70,70, 0.75)')};
-  color: ${(props) => (props.isNavColored ? 'var(--header-gray)' : 'white')};
+  background: white;
+  color: gray;
   z-index: 20;
-  opacity: ${(props) => (props.state === 'entered' ? 1 : 0)};
+  /* opacity: ${(props) => (props.state === 'entered' ? 1 : 0)}; */
 
   transition: opacity 0.2s ease-in-out, background-color 1.4s;
 
   /* & div:nth-child(8) {
     border-bottom: none;
   } */
+
+  @media ${device.minLg} {
+    display: none;
+  }
 `;
 
 const HamburgerLink = styled.div`
   width: 100%;
   padding: 1rem 0;
   cursor: pointer;
-  border-bottom: ${(props) =>
-    props.isNavColored ? '2px solid var(--heart)' : '2px solid var(--header-gray)'};
+  border-bottom: gray;
 
   &:hover {
-    background: ${(props) =>
-      props.isNavColored ? 'rgba(235,235,235)' : 'rgba(100,100,100, 0.75)'};
+    background: 'rgba(235,235,235)';
   }
 `;
 
 const BurgerLine = styled.div`
   width: 40px;
   height: 3px;
-  background-color: ${(props) => (props.isNavColored ? 'var(--header-gray)' : 'white')};
+  background-color: gray;
   margin: 8px 0;
   border-radius: 5px;
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.13), 0 2px 3px 0 rgba(0, 0, 0, 0.15);
@@ -269,7 +274,7 @@ const BurgerLine = styled.div`
 `;
 
 const NavLink = styled.div`
-  margin: 1.9rem 0.85rem;
+  margin: 1rem 0.85rem;
   text-transform: uppercase;
   font-size: 0.8rem;
   font-weight: 700;
@@ -293,4 +298,11 @@ const LogoContainer = styled.div`
   padding: 0.3rem 0.7rem;
   border-radius: 10px;
   cursor: pointer;
+
+  @media ${device.maxLg} {
+    font-size: 0.9rem;
+  }
+  @media ${device.maxSm} {
+    font-size: 0.8rem;
+  }
 `;
