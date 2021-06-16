bc92cf5843da7deb866bdebea3f9e1b776ff099e
francis <francis@aahcs.com>
2021-06-16 15:39:41 -0400

    feat: [WIP] Add cards, MVP to demo

--- components/page-index/BeautyCosmeticsRow.tsx
+++ components/page-index/BeautyCosmeticsRow.tsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 import Slider from 'react-slick';
 import Modal from 'react-modal';
+import styled from 'styled-components';
 
 import { Container, PictureContainer, Card, CardDialog, RowTitle } from 'components/utils/styled';
 import { verticalSliderProps } from 'components/utils';
 
 export default function BeautyCosmesticsRow() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [influencerData, setInfluencerData] = useState<any>(null);
 
   return (
     <Container>
@@ -17,7 +19,13 @@ export default function BeautyCosmesticsRow() {
 
       <Slider {...verticalSliderProps}>
         {profiles.map((influencer, key) => (
-          <PictureContainer key={key} onClick={() => setIsModalOpen(true)}>
+          <PictureContainer
+            key={key}
+            onClick={() => {
+              setInfluencerData(influencer);
+              setIsModalOpen(true);
+            }}
+          >
             <Card>
               <Image
                 src={`/images/influencer-index/${influencer.pic}`}
@@ -61,11 +69,101 @@ export default function BeautyCosmesticsRow() {
         ariaHideApp={false}
         style={{ overlay: { zIndex: 1000 } }}
       >
-        <h2>Welcome to the jungle</h2>
+        <ModalHead>
+          <button onClick={() => setIsModalOpen(false)}>X</button>
+        </ModalHead>
+
+        <Image
+          src={`/images/influencer-index/${influencerData.pic}`}
+          alt="Picture of the author"
+          layout="responsive"
+          width={310}
+          height={440}
+          quality={50}
+        />
+
+        <ModalContent>
+          <h4>{influencerData.name}</h4>
+
+          {/* TODO: Need to make `industry an actual array,` */}
+          {influencerData.industry[0].split(',').map((ind: any, key: any) => (
+            <Industrybadge key={key}>{ind}</Industrybadge>
+          ))}
+
+          <p style={{ fontSize: 12 }}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui fuga soluta porro voluptas
+            nesciunt excepturi reprehenderit quod aliquid expedita! Vero tenetur minima quae dolorum
+            aut natus. Harum, vel nam? Obcaecati.
+          </p>
+
+          <div style={{ display: 'flex' }}>
+            <SaveButton>Save</SaveButton>
+            <ContactButton>Contact</ContactButton>
+          </div>
+        </ModalContent>
       </Modal>
     </Container>
   );
 }
+const ContactButton = styled.button`
+  display: block;
+  width: 100%;
+  background: #e20046;
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  margin: 1rem 0.5rem 1rem 0;
+  font-size: 1rem;
+`;
+
+const SaveButton = styled(ContactButton)`
+  background: #f1f2f3;
+  color: #000;
+  border: 1px solid #babfc4;
+`;
+
+const Industrybadge = styled.div`
+  background: #e20046;
+  display: inline-block;
+  color: white;
+  padding: 0.25rem 0.75rem;
+  border-radius: 50px;
+  text-align: center;
+  text-transform: capitalize;
+  margin: 1rem 0.5rem 1rem 0;
+  font-size: 12px;
+`;
+
+const ModalHead = styled.div`
+  position: absolute;
+  top: 7px;
+  right: 7px;
+  margin-bottom: 1rem;
+  z-index: 1;
+
+  & button {
+    padding: 0.5rem 0.7rem;
+    background: #ff0051;
+    color: white;
+    border: 1px solid white;
+    border-radius: 50px;
+  }
+`;
+
+const ModalContent = styled.div`
+  padding: 1rem;
+
+  & h4 {
+    background: #000;
+    padding: 0.5rem 1rem;
+    color: white;
+    text-transform: uppercase;
+
+    text-align: center;
+    margin: 0;
+  }
+`;
 
 const profiles = [
   {
