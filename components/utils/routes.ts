909534aced38b528557b2e4c256029fad8bba3c3
Francis Calizo <francisacalizo@gmail.com>
2021-06-23 22:44:07 -0400

    feat: create routes in utils

---
+++ components/utils/routes.ts
@@ -0,0 +1,8 @@
+export const routes = [
+  { title: 'Categories', path: '/categories' },
+  { title: 'The Talent', path: '/talent' },
+  { title: 'Your Cast', path: '/cast' },
+  { title: 'Events', path: '/events' },
+  { title: 'Login', path: '/login' },
+  { title: 'Contact', path: '/contact' },
+];
