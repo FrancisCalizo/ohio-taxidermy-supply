49ecea7425b4d73c517f835d458fc9ccf69d3abf
francis <francis@aahcs.com>
2021-06-25 17:19:08 -0400

    fix: remove lists

--- components/pages/dashboard/discover/index.tsx
+++ components/pages/dashboard/discover/index.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
 
-import CookingKitchenRow from 'components/page-index/CookingKitchenRow';
-import TravelRow from 'components/page-index/TravelRow';
-import DiyRow from 'components/page-index/DiyRow';
-import DramaRow from 'components/page-index/DramaRow';
-import FitnessSportsRow from 'components/page-index/FitnessSportsRow';
-import HomeGardenRow from 'components/page-index/HomeGardenRow';
-
 export default function Discover() {
   return (
     <>
-      <CookingKitchenRow />
-      <DiyRow />
-      <DramaRow />
-      <FitnessSportsRow />
-      <HomeGardenRow />
-      <TravelRow />
+      <h1>Discover Stuff</h1>
     </>
   );
 }
