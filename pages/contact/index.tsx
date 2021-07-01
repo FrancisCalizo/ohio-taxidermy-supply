3d71000c79cee5729b607306c6de8820c25d3731
francis <francis@aahcs.com>
2021-07-01 07:35:30 -0400

    fix: implement responsive design on contact. Styling changes

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -7,45 +7,88 @@ import SiteLayout from 'components/layout/SiteLayout';
 
 export default function index() {
   return (
-    <div style={{ height: '100%', background: 'red' }}>
-      <FlexContainer>
-        <div>
-          <Image
-            src={'/images/contact.jpg'}
-            alt="about-image"
-            layout="responsive"
-            width={600}
-            height={450}
-            quality={50}
-          />
-        </div>
-        <div>
-          <FormContainer>
-            {/* eslint-disable-next-line react/no-unescaped-entities */}
-            <Title>Let's get in touch!</Title>
-            <div className="name-container">
-              <Input placeholder="First Name*" />
-              <Input placeholder="Last Name" />
-            </div>
-            <Input placeholder="Email Address*" />
-            <Input placeholder="Company*" />
-            <Input placeholder="Phone Number*" />
-            <Input placeholder="Country" />
-            <TextArea placeholder="Message" rows={5} />
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-              <SaveButton>Submit</SaveButton>
-            </div>
-          </FormContainer>
-        </div>
-      </FlexContainer>
-    </div>
+    <FlexContainer>
+      <div>
+        <Image
+          src={'/images/contact.jpg'}
+          alt="about-image"
+          layout="responsive"
+          width={600}
+          height={450}
+          quality={50}
+        />
+      </div>
+      <div>
+        <FormContainer>
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          <Title>Let's get in touch!</Title>
+          <div className="name-container">
+            <Input placeholder="First Name*" />
+            <Input placeholder="Last Name" />
+          </div>
+          <Input placeholder="Email Address*" />
+          <Input placeholder="Company*" />
+          <Input placeholder="Phone Number*" />
+          <Input placeholder="Country" />
+          <TextArea placeholder="Message*" rows={5} />
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <SaveButton>Submit</SaveButton>
+          </div>
+        </FormContainer>
+      </div>
+    </FlexContainer>
   );
 }
 
+const FlexContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 2rem;
+  color: ${(props) => props.theme.colors.gray};
+  background: #f2869b;
+  color: white;
+  height: 100vh;
+
+  & > div:nth-child(1) {
+    margin-right: 1rem;
+    width: 60%;
+
+    @media (max-width: 1200px) {
+      width: 50%;
+    }
+
+    @media (max-width: 960px) {
+      width: 100%;
+      margin: 1rem 1rem 2rem;
+    }
+  }
+
+  & > div:nth-child(2) {
+    margin-left: 1rem;
+    width: 40%;
+    margin-right: 2rem;
+
+    @media (max-width: 1200px) {
+      width: 50%;
+    }
+
+    @media (max-width: 960px) {
+      width: 100%;
+      margin-bottom: 2rem;
+      margin-right: 1rem;
+    }
+  }
+
+  @media (max-width: 960px) {
+    flex-direction: column;
+    height: 100%;
+  }
+`;
+
 const Input = styled.input`
   display: block;
-  font-size: 18px;
-  padding: 0.5rem;
+  font-size: ${(props) => props.theme.input.fontSize};
+  padding: ${(props) => props.theme.input.padding};
   margin: 1rem auto;
   width: 100%;
   border: 1px solid lightgray;
@@ -55,8 +98,8 @@ const Input = styled.input`
 const TextArea = styled.textarea<{ rows: number }>`
   width: 100%;
   display: block;
-  font-size: 18px;
-  padding: 0.5rem;
+  font-size: ${(props) => props.theme.input.fontSize};
+  padding: ${(props) => props.theme.input.padding};
   margin: 1rem auto;
   border: 1px solid lightgray;
   border-radius: 4px;
@@ -88,11 +131,12 @@ const Title = styled.h1`
   text-align: center;
   margin: 0.5rem 0 1.5rem;
   color: #000;
+  font-family: Shadows Into Light;
 `;
 
 const SaveButton = styled.button`
   display: block;
-  background: ${(props) => props.theme.colors.pink};
+  background: #f2869b;
   color: white;
   border: none;
   padding: 0.75rem 3rem;
@@ -100,45 +144,10 @@ const SaveButton = styled.button`
   margin: 1rem 0.5rem 1rem 0;
   font-size: 1.3rem;
   cursor: pointer;
-  box-shadow: ${(props) => props.theme.button.boxShadow};
+  box-shadow: #f2869b;
 
   &:hover {
-    background: ${(props) => darken(0.1, props.theme.colors.pink)};
-  }
-`;
-
-const FlexContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 0 2rem;
-  color: ${(props) => props.theme.colors.gray};
-  background: #f2869b;
-  color: white;
-  height: 100vh;
-
-  & > div:nth-child(1) {
-    margin-right: 1rem;
-    width: 60%;
-
-    @media (max-width: 960px) {
-      width: 100%;
-      margin: 1rem 1rem 2rem;
-    }
-  }
-
-  & > div:nth-child(2) {
-    margin-left: 1rem;
-    width: 40%;
-
-    @media (max-width: 960px) {
-      width: 100%;
-      margin-bottom: 2rem;
-    }
-  }
-
-  @media (max-width: 960px) {
-    flex-direction: column;
-    margin-bottom: 4rem;
+    background: ${darken(0.1, '#f2869b')};
   }
 `;
 
