852bf171e10571bcc1da3a79baa453c9209be8da
francis <francis@aahcs.com>
2021-07-02 15:26:02 -0400

    fix: remove extra Dashboard Layout causing style issues

--- components/pages/dashboard/discover/index.tsx
+++ components/pages/dashboard/discover/index.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 
-import DashboardLayout from 'components/layout/dashboard/DashboardLayout';
 import OverviewComponent from 'components/pages/dashboard/overview';
 
 export default function Discover() {
-  return (
-    <DashboardLayout>
-      <OverviewComponent />
-    </DashboardLayout>
-  );
+  return <OverviewComponent />;
 }
