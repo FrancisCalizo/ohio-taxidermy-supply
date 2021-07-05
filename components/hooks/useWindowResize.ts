9439daf68c07afdd34832d7c331749f9dab9d8d5
francis <francis@aahcs.com>
2021-07-05 12:30:41 -0400

    fix: wait until component is fully rendered to use the window object within the useWindowResize hook

--- components/hooks/useWindowResize.ts
+++ components/hooks/useWindowResize.ts
@@ -4,6 +4,10 @@ export default function useWindowResize() {
   const [size, setSize] = React.useState([0, 0]);
 
   React.useEffect(() => {
+    // Window is undefined in NextJS until component is rendered. Therefore
+    // we must initially set the window size after the component has fully rendered
+    setSize([window.innerWidth, window.innerHeight]);
+
     const handleResize = () => {
       setSize([window.innerWidth, window.innerHeight]);
     };
