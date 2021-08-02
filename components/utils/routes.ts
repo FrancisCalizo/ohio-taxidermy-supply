cabec6474c7d6af7eefc313c045d9e158fdbed6f
francis <francis@aahcs.com>
2021-08-02 12:17:43 -0400

    fix: change routes to 5 main

--- components/utils/routes.ts
+++ components/utils/routes.ts
@@ -1,10 +1,9 @@
 export const routes = [
-  { title: 'Categories', path: '/categories' },
-  { title: 'The Talent', path: '/talent' },
-  { title: 'Your Cast', path: '/cast' },
+  { title: 'Talent', path: '/talent' },
+  { title: 'Clients', path: '/clients' },
   { title: 'Events', path: '/events' },
-  { title: 'Login', path: '/login' },
-  { title: 'Contact', path: '/contact' },
+  { title: 'Vendors', path: '/vendors' },
+  { title: 'Travel', path: '/travel' },
 ];
 
 export const middleRoutes = [
@@ -16,51 +15,21 @@ export const footerHeaders = ['About', 'Privacy', 'What We Do', 'Resources'];
 
 export const footerRoutes = [
   [
-    {
-      title: 'Who We Are',
-      path: '/whoweare',
-    },
-    {
-      title: 'Our Mission',
-      path: '/mission',
-    },
-    {
-      title: 'Contact',
-      path: '/contact',
-    },
+    { title: 'Who We Are', path: '/whoweare' },
+    { title: 'Our Mission', path: '/mission' },
+    { title: 'Contact', path: '/contact' },
   ],
   [
-    {
-      title: 'Privacy',
-      path: '/privacy',
-    },
-    {
-      title: 'Terms and Conditions',
-      path: '/termsandconditions',
-    },
+    { title: 'Privacy', path: '/privacy' },
+    { title: 'Terms and Conditions', path: '/termsandconditions' },
   ],
   [
-    {
-      title: 'Strategy',
-      path: '/strategy',
-    },
-    {
-      title: 'Data',
-      path: '/data',
-    },
-    {
-      title: 'Technology',
-      path: '/technology',
-    },
+    { title: 'Strategy', path: '/strategy' },
+    { title: 'Data', path: '/data' },
+    { title: 'Technology', path: '/technology' },
   ],
   [
-    {
-      title: 'Latest',
-      path: '/latest',
-    },
-    {
-      title: 'Blog',
-      path: '/blog',
-    },
+    { title: 'Latest', path: '/latest' },
+    { title: 'Blog', path: '/blog' },
   ],
 ];
