2dfda6a0ca5d3b1df4cc67a486cb55fa53b0426b
francis <francis@aahcs.com>
2021-08-17 08:26:42 -0400

    feat: [WIP] add text to campaign modal

--- components/pages/clients/CampaignModal.tsx
+++ components/pages/clients/CampaignModal.tsx
@@ -33,6 +33,35 @@ export default function CampaignModal() {
               objectFit="cover"
             />
           </ClientImageContainer>
+          <ClientDescriptionContainer>
+            <h3 className="heading">Lorem ipsum dolor sit amet.</h3>
+
+            <h3 className="sub-heading">Campaign Description</h3>
+
+            <p className="description-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptas eveniet
+              labore porro laborum non? Alias incidunt doloremque vero, deleniti illo,
+              exercitationem molestias, commodi delectus voluptas veniam aspernatur quae nihil quis
+              mollitia. Commodi nihil culpa voluptatem odit praesentium debitis facilis dolore eum
+              sequi cupiditate voluptatum excepturi, ab pariatur. Aperiam, ipsam!
+            </p>
+
+            <h3 className="sub-heading">The Demographics</h3>
+
+            <p className="description-text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod voluptatibus enim,
+              officiis beatae eveniet nihil quia alias, corrupti odio, sit vero quasi distinctio
+              animi a.
+            </p>
+
+            <p className="description-text">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod voluptatibus enim,
+              officiis beatae eveniet nihil quia alias, corrupti odio, sit vero quasi distinctio
+              animi a.
+            </p>
+
+            <h3 className="sub-heading">The Cast</h3>
+          </ClientDescriptionContainer>
         </MainContainer>
       </Modal>
     </div>
@@ -47,6 +76,25 @@ const ClientImageContainer = styled.div`
   position: relative;
   height: 80vh;
   width: 50%;
+`;
+
+const ClientDescriptionContainer = styled.div`
+  padding: 2rem;
+  width: 50%;
+
+  h3 {
+    text-transform: uppercase;
+  }
+
+  h3.sub-heading {
+    font-size: 1rem;
+  }
+
+  p.description-text {
+    color: ${({ theme }) => theme.colors.gray};
+    font-size: 0.85rem;
+    line-height: 1.5rem;
+  }
 `;
 
 const mobileModal = { top: 0, bottom: 0, left: 0, right: 0 };
