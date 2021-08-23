ee60b38950c131be6a52e8d3e2a3532fe8a8b276
francis <francis@aahcs.com>
2021-08-23 14:19:54 -0400

    feat: add placeholder pages for travel and vending

---
+++ pages/travel/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import SiteLayout from 'components/layout/SiteLayout';
+import { PageTitle } from 'components/utils/styled';
+
+export default function Travel() {
+  return (
+    <MainContainer>
+      <div className="title-container">
+        <PageTitle>Vending</PageTitle>
+      </div>
+    </MainContainer>
+  );
+}
+
+const MainContainer = styled.div`
+  padding: 0 1rem 1rem;
+
+  & > .title-container {
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+Travel.getLayout = (page: any) => <SiteLayout>{page}</SiteLayout>;
