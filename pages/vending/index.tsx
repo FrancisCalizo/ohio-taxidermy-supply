ffe218dbf625d097ecf36dd173122a096f61cf95
francis <francis@aahcs.com>
2021-08-26 08:09:57 -0400

    feat: start implement vending page add filters

--- pages/vending/index.tsx
+++ pages/vending/index.tsx
@@ -1,16 +1,52 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import styled from 'styled-components';
+import { useQuery } from 'react-query';
 
 import SiteLayout from 'components/layout/SiteLayout';
+import VendingFilter from 'components/pages/vending/VendingFilter';
 import { PageTitle } from 'components/utils/styled';
+import { getTravelCoupons } from 'components/api/travel';
+
+export const VendingContext = createContext({} as any);
 
 export default function Vending() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [companyFilters, setCompanyFilters] = useState<any>([]);
+  const [categoryFilters, setCategoryFilters] = useState<any>([]);
+  const [postTypeFilters, setPostTypeFilters] = useState<any>([]);
+
+  const { data, isLoading } = useQuery('travelCoupons', getTravelCoupons);
+
+  if (isLoading) <h1>Loading</h1>;
+
   return (
-    <MainContainer>
-      <div className="title-container">
-        <PageTitle>Vending</PageTitle>
-      </div>
-    </MainContainer>
+    <VendingContext.Provider
+      value={{
+        isModalOpen,
+        setIsModalOpen,
+        data,
+        companyFilters,
+        setCompanyFilters,
+        categoryFilters,
+        setCategoryFilters,
+        postTypeFilters,
+        setPostTypeFilters,
+        // selectedCoupon,
+        // setSelectedCoupon,
+      }}
+    >
+      <MainContainer>
+        <div className="title-container">
+          <PageTitle>Vending</PageTitle>
+        </div>
+
+        <div className="filter-container">
+          <div className="filter">
+            <VendingFilter />
+          </div>
+        </div>
+      </MainContainer>
+    </VendingContext.Provider>
   );
 }
 
@@ -20,6 +56,22 @@ const MainContainer = styled.div`
   & > .title-container {
     display: flex;
     justify-content: center;
+    margin-bottom: -2rem;
+  }
+
+  .filter-container {
+    display: flex;
+    justify-content: center;
+    margin: 0 0 1rem;
+
+    & .filter {
+      width: 400px;
+      margin: 0.5rem 0;
+
+      @media (max-width: 800px) {
+        width: 100%;
+      }
+    }
   }
 `;
 
