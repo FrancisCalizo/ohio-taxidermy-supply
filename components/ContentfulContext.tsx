8f6737fd561a0b228e6799c042111609e9a78848
francis <francis@aahcs.com>
2021-09-24 15:45:37 -0400

    fix: comment out createClient access Tokens for time being

--- components/ContentfulContext.tsx
+++ components/ContentfulContext.tsx
@@ -1,26 +1,26 @@
-import React, { createContext, useContext } from 'react';
-import { createClient as createClientContent } from 'contentful';
-import { createClient as createClientManagement } from 'contentful-management';
+// import React, { createContext, useContext } from 'react';
+// import { createClient as createClientContent } from 'contentful';
+// import { createClient as createClientManagement } from 'contentful-management';
 
-const clientContent = createClientContent({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID as string,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY as string,
-});
+// const clientContent = createClientContent({
+//   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID as string,
+//   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY as string,
+// });
 
-const clientManagement = createClientManagement({
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_ACCESS_KEY as string,
-});
+// const clientManagement = createClientManagement({
+//   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_ACCESS_KEY as string,
+// });
 
-const ContentfulContext = createContext({});
+// const ContentfulContext = createContext({});
 
-interface ProviderProps {
-  children: React.ReactNode;
-}
+// interface ProviderProps {
+//   children: React.ReactNode;
+// }
 
-export const ContentfulProvider = ({ children }: ProviderProps) => (
-  <ContentfulContext.Provider value={{ clientContent, clientManagement }}>
-    {children}
-  </ContentfulContext.Provider>
-);
+// export const ContentfulProvider = ({ children }: ProviderProps) => (
+//   <ContentfulContext.Provider value={{ clientContent, clientManagement }}>
+//     {children}
+//   </ContentfulContext.Provider>
+// );
 
-export const useContentful: any = () => useContext(ContentfulContext);
+// export const useContentful: any = () => useContext(ContentfulContext);
--- components/api/contentful.ts
+++ components/api/contentful.ts
@@ -1,11 +1,11 @@
-import { createClient as createClientContent } from 'contentful';
-import { createClient as createClientManagement } from 'contentful-management';
+// import { createClient as createClientContent } from 'contentful';
+// import { createClient as createClientManagement } from 'contentful-management';
 
-export const clientContent = createClientContent({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID as string,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY as string,
-});
+// export const clientContent = createClientContent({
+//   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID as string,
+//   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY as string,
+// });
 
-export const clientManagement = createClientManagement({
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_ACCESS_KEY as string,
-});
+// export const clientManagement = createClientManagement({
+//   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_MANAGEMENT_ACCESS_KEY as string,
+// });
